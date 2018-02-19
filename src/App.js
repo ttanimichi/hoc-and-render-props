044020871201b0da3a653665a5b3e33e7c3669a8
Tsukuru Tanimichi <info+git@ttanimichi.com>
2018-02-19 19:24:27 +0900

    Implement a simple counter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 
-function WrappedComponent(props) {
-  return (
-    <div>
-      <div>{JSON.stringify(props)}</div>
-    </div>
-  );
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { value: 0 };
+  }
 
-function higherOrderComponent(WrappedComponent) {
-  return (props) => {
-    return <WrappedComponent piyo="piyo" {...props} />;
-  };
-}
+  handleClick() {
+    this.setState(({ value }) => {
+      return { value: value + 1 };
+    });
+  }
 
-export default class App extends Component {
   render() {
-    const EnhancedComponent = higherOrderComponent(WrappedComponent);
-
     return (
-      <div className="App">
-        <h1>App</h1>
-        <EnhancedComponent foo="foo" />
+      <div>
+        <div>count: {this.state.value}</div>
+        <button onClick={this.handleClick}>+1</button>
       </div>
     );
   }
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  );
 }
