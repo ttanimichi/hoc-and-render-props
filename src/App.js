f15418acd9cd0993c0dea089a50e40ed85325d04
Tsukuru Tanimichi <info+git@ttanimichi.com>
2018-02-20 12:37:01 +0900

    HOC minimal example

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { value: 0 };
-  }
+function CounterComponent({ value, handleClick }) {
+  return (
+    <div>
+      <div>count: {value}</div>
+      <button onClick={handleClick}>+1</button>
+    </div>
+  );
+}
 
-  handleClick() {
-    this.setState(({ value }) => {
-      return { value: value + 1 };
-    });
-  }
+function higherOrderComponent(CounterComponent) {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind(this);
+      this.state = { value: 0 };
+    }
 
-  render() {
-    return (
-      <div>
-        <div>count: {this.state.value}</div>
-        <button onClick={this.handleClick}>+1</button>
-      </div>
-    );
+    handleClick() {
+      this.setState(({ value }) => {
+        return { value: value + 1 };
+      });
+    }
+
+    render() {
+      return <CounterComponent value={this.state.value} handleClick={this.handleClick} />;
+    }
   }
 }
+
+const CounterContainer = higherOrderComponent(CounterComponent);
 
 export default function App() {
   return (
     <div>
       <h1>Counter</h1>
-      <Counter />
+      <CounterContainer />
     </div>
   );
 }
